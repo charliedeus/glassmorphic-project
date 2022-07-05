b0d4771449e74efa0f8d8f0b5c48291f2e537674
charliedeus <charliedeus@gmail.com>
2022-07-05 17:05:08 -0300

    Adding links to menu group

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,36 @@
+import Link from 'next/link';
 import Logo from '../Logo';
 
 export function Header() {
   return (
     <header className="w-[1440px] max-w-[1440px] h-[14rem] mx-auto flex items-center justify-between fixed top-0 left-0 right-0">
-      <Logo />
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
 
       <nav>
         <ul className="flex gap-16 font-bold">
           <li>
-            <a href="/">Features</a>
+            <Link href="/">
+              <a>Features</a>
+            </Link>
           </li>
           <li>
-            <a href="/">Screenshots</a>
+            <Link href="/">
+              <a>Screenshots</a>
+            </Link>
           </li>
           <li>
-            <a href="/">Partners</a>
+            <Link href="/">
+              <a>Partners</a>
+            </Link>
           </li>
           <li>
-            <a href="/">About</a>
+            <Link href="/">
+              <a>About</a>
+            </Link>
           </li>
         </ul>
       </nav>
